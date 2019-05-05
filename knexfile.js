039fc938573face75ac46fadcd3ed45f87982810
Arpita Sinha <sinha.arpita@gmail.com>
2019-05-04 17:41:52 -0700

    Postgress DB migrations

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 const localPgConnection = {
   host: 'localhoat',
-  database : 'splitBill4',
+  database : 'splitbill',
   user : 'student',
   password : 'hired'
 }
@@ -9,11 +9,8 @@ const prodDBConnection = process.env.DATABASE_URL || localPgConnection;
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./splitBill4.sqlite3"
-    },
-    useNullAsDefault: true,
+    client: "pg",
+    connection: prodDBConnection,
     migrations: {
       directory: "./data/migrations"
     },
